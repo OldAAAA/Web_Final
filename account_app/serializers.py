e594053c286d9f30dabc1373a60c7926f2f18215
alapingping <16301111@bjtu.edu.cn>
2018-12-10 14:37:12 +0800

    add api back field username

--- account_app/serializers.py
+++ account_app/serializers.py
@@ -6,4 +6,4 @@ from .models import User
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('email','last_check')
+        fields = ('email','last_check','username')
