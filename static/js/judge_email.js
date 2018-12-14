b359d42b4f2cc1ed095e4e2071cbb126716c586f
JiahangOK <2973493373@qq.com>
2018-12-14 11:34:22 +0800

    update

--- static/js/judge_email.js
+++ static/js/judge_email.js
@@ -22,6 +22,7 @@ function isEmailorUsername_login()
 
 function isEmail_update()
 {
+	document.getElementById("success_div").innerHTML="";
     var email=document.getElementById("email").value;
 	var pattern= /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
 		strEmail=pattern.test(email);
