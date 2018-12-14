a77272dc0783961cf4043e4fd1a798861c484392
JiahangOK <2973493373@qq.com>
2018-12-14 09:15:01 +0800

    update

--- static/js/judge.js
+++ static/js/judge.js
@@ -1,184 +1,181 @@
-function isEmail()
-{
-    var email=document.getElementById("email").value;
-	var pattern= /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
-		strEmail=pattern.test(email);
-    	if (strEmail)
-    	{
-			document.getElementById("checkemail").style.color="green";
-        	document.getElementById("checkemail").style.fontSize="14px";
-			document.getElementById("checkemail").innerHTML="✔";
-			document.getElementById("email").style.borderColor="rgb(29,200,287)";
-			return true;
-    	}
-		else
-    	{
-			document.getElementById("checkemail").style .color="red";//设置邮箱不可用时的字体颜色
-			document.getElementById("checkemail").style.fontSize="14px";
-			document.getElementById("checkemail").innerHTML="Invalid email!";
-			document.getElementById("email").style.borderColor="red";
-			return false;
-		}
+function isEmail() {
+    var email = document.getElementById("email").value;
+    var pattern = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+    strEmail = pattern.test(email);
+    if (strEmail) {
+        document.getElementById("checkemail").style.color = "green";
+        document.getElementById("checkemail").style.fontSize = "14px";
+        document.getElementById("checkemail").innerHTML = "✔";
+        document.getElementById("email").style.borderColor = "rgb(29,200,287)";
+        return true;
+    }
+    else {
+        document.getElementById("checkemail").style.color = "red";//设置邮箱不可用时的字体颜色
+        document.getElementById("checkemail").style.fontSize = "14px";
+        document.getElementById("checkemail").innerHTML = "Invalid email!";
+        document.getElementById("email").style.borderColor = "red";
+        return false;
+    }
 }
 
-function checkpassword(){
-	var password =document.getElementById("password").value;
+function checkpassword() {
+    var password = document.getElementById("password").value;
 
-	if(password.length<6){
-		document.getElementById("checkpassword").style.color="red";
-        document.getElementById("checkpassword").style.fontSize="14px";
-		document.getElementById("checkpassword").innerHTML="Password too short (6 chars. min)";
-		document.getElementById("password").style.borderColor="red";
-	    return false;
-	}else{
-		document.getElementById("checkpassword").style.color="green";
-        document.getElementById("checkpassword").style.fontSize="14px";
-		document.getElementById("checkpassword").innerHTML="✔";
-		document.getElementById("password").style.borderColor="rgb(29,200,287)";
-		return true;
-	}
+    if (password.length < 6) {
+        document.getElementById("checkpassword").style.color = "red";
+        document.getElementById("checkpassword").style.fontSize = "14px";
+        document.getElementById("checkpassword").innerHTML = "Password too short (6 chars. min)";
+        document.getElementById("password").style.borderColor = "red";
+        return false;
+    } else {
+        document.getElementById("checkpassword").style.color = "green";
+        document.getElementById("checkpassword").style.fontSize = "14px";
+        document.getElementById("checkpassword").innerHTML = "✔";
+        document.getElementById("password").style.borderColor = "rgb(29,200,287)";
+        return true;
+    }
 
     // if(password.length<6){
     //     document.getElementById("checkpassword").style.color="red";
     //     document.getElementById("checkpassword").style.fontSize="14px";
-	// 	document.getElementById("checkpassword").innerHTML="Password too short (6 chars. min)";
-	// 	document.getElementById("password").style.borderColor="red";
-	//     return false;
+    // 	document.getElementById("checkpassword").innerHTML="Password too short (6 chars. min)";
+    // 	document.getElementById("password").style.borderColor="red";
+    //     return false;
     // }else if(document.getElementById("password").value==null){
     // 	document.getElementById("checkpassword").style.color="red";
     // 	document.getElementById("checkpassword").style.fontSize="14px";
-	// 	document.getElementById("checkpassword").innerHTML="Password too short (6 chars. min)";
-	// 	document.getElementById("password").style.borderColor="red";
-	//     return false;
-	// }else if(document.getElementById("password_confirmation").value!=document.getElementById("password").value){
-	// 	document.getElementById("checkpassword").style.color="red";
-	// 	document.getElementById("checkpassword").style.fontSize="14px";
-	// 	document.getElementById("checkpassword").innerHTML="Password mismatch!";
-	// 	document.getElementById("password_confirmation").style.borderColor="red";
-	// 	return false;
-	// }else{
-	// 	document.getElementById("checkpassword").innerHTML="";
-	// 	document.getElementById("password").style.borderColor="rgb(29,200,287)";
-	// 	document.getElementById("password_confirmation").style.borderColor="rgb(29,200,287)";
-	// 	// document.getElementById("check").innerHTML="";
-	// 	// document.getElementById("check").style.display="none";
-	// 	// document.getElementById("check").style.display="inline";
-	// }
+    // 	document.getElementById("checkpassword").innerHTML="Password too short (6 chars. min)";
+    // 	document.getElementById("password").style.borderColor="red";
+    //     return false;
+    // }else if(document.getElementById("password_confirmation").value!=document.getElementById("password").value){
+    // 	document.getElementById("checkpassword").style.color="red";
+    // 	document.getElementById("checkpassword").style.fontSize="14px";
+    // 	document.getElementById("checkpassword").innerHTML="Password mismatch!";
+    // 	document.getElementById("password_confirmation").style.borderColor="red";
+    // 	return false;
+    // }else{
+    // 	document.getElementById("checkpassword").innerHTML="";
+    // 	document.getElementById("password").style.borderColor="rgb(29,200,287)";
+    // 	document.getElementById("password_confirmation").style.borderColor="rgb(29,200,287)";
+    // 	// document.getElementById("check").innerHTML="";
+    // 	// document.getElementById("check").style.display="none";
+    // 	// document.getElementById("check").style.display="inline";
+    // }
 
 }
 
 function checkpasswordconfirm() {
-	var password =document.getElementById("password").value;
-	var passwordconfirm=document.getElementById("password_confirmation").value;
+    var password = document.getElementById("password").value;
+    var passwordconfirm = document.getElementById("password_confirmation").value;
 
-	if(passwordconfirm!=password){
-		document.getElementById("checkpasswordconfirm").style.color="red";
-		document.getElementById("checkpasswordconfirm").style.fontSize="14px";
-		document.getElementById("checkpasswordconfirm").innerHTML="Password mismatch!";
-		document.getElementById("password_confirmation").style.borderColor="red";
-		return false;
-	}else{
-		document.getElementById("checkpasswordconfirm").style.color="green";
-        document.getElementById("checkpasswordconfirm").style.fontSize="14px";
-		document.getElementById("checkpasswordconfirm").innerHTML="✔";
-		document.getElementById("password_confirmation").style.borderColor="rgb(29,200,287)";
-		return true;
-	}
+    if (passwordconfirm != password) {
+        document.getElementById("checkpasswordconfirm").style.color = "red";
+        document.getElementById("checkpasswordconfirm").style.fontSize = "14px";
+        document.getElementById("checkpasswordconfirm").innerHTML = "Password mismatch!";
+        document.getElementById("password_confirmation").style.borderColor = "red";
+        return false;
+    } else {
+        document.getElementById("checkpasswordconfirm").style.color = "green";
+        document.getElementById("checkpasswordconfirm").style.fontSize = "14px";
+        document.getElementById("checkpasswordconfirm").innerHTML = "✔";
+        document.getElementById("password_confirmation").style.borderColor = "rgb(29,200,287)";
+        return true;
+    }
 }
 
 function isUsername() {
-	var username =document.getElementById("username").value;
-	if(username==""){
-		document.getElementById("checkusername").style.color="red";
-		document.getElementById("checkusername").style.fontSize="14px";
-		document.getElementById("checkusername").innerHTML="Please enter username!";
-		document.getElementById("username").style.borderColor="red";
-		return false;
-	}else{
-		document.getElementById("checkusername").style.color="green";
-        document.getElementById("checkusername").style.fontSize="14px";
-		document.getElementById("checkusername").innerHTML="✔";
-		document.getElementById("username").style.borderColor="rgb(29,200,287)";
-		return true;
-	}
+    var username = document.getElementById("username").value;
+    var usern = /^[0-9a-z_]{1,}$/;
+    if (!usern.test(username)) {
+        document.getElementById("checkusername").style.color = "red";
+        document.getElementById("checkusername").style.fontSize = "14px";
+        document.getElementById("checkusername").innerHTML = "Invalid username!";
+        document.getElementById("username").style.borderColor = "red";
+        return false;
+    } else {
+        document.getElementById("checkusername").style.color = "green";
+        document.getElementById("checkusername").style.fontSize = "14px";
+        document.getElementById("checkusername").innerHTML = "✔";
+        document.getElementById("username").style.borderColor = "rgb(29,200,287)";
+        return true;
+    }
 }
 
-function isall(){
-	aa=1;
+function isall() {
+    aa = 1;
 
-	var email=document.getElementById("email").value;
-	var pattern= /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
-		strEmail=pattern.test(email);
-    	if (strEmail)
-    	{
-    		document.getElementById("checkemail").style.color="green";
-        	document.getElementById("checkemail").style.fontSize="14px";
-			document.getElementById("checkemail").innerHTML="✔";
-			document.getElementById("email").style.borderColor="rgb(29,200,287)";
+    var email = document.getElementById("email").value;
+    var pattern = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+    strEmail = pattern.test(email);
+    if (strEmail) {
+        document.getElementById("checkemail").style.color = "green";
+        document.getElementById("checkemail").style.fontSize = "14px";
+        document.getElementById("checkemail").innerHTML = "✔";
+        document.getElementById("email").style.borderColor = "rgb(29,200,287)";
 
-    	}
-		else
-    	{
-			document.getElementById("checkemail").style .color="red";//设置邮箱不可用时的字体颜色
-			document.getElementById("checkemail").style.fontSize="14px";
-			document.getElementById("checkemail").innerHTML="Invalid email!";
-			document.getElementById("email").style.borderColor="red";
-			aa=0;
-		}
+    }
+    else {
+        document.getElementById("checkemail").style.color = "red";//设置邮箱不可用时的字体颜色
+        document.getElementById("checkemail").style.fontSize = "14px";
+        document.getElementById("checkemail").innerHTML = "Invalid email!";
+        document.getElementById("email").style.borderColor = "red";
+        aa = 0;
+    }
 
-	var password =document.getElementById("password").value;
+    var password = document.getElementById("password").value;
 
-	if(password.length<6){
-		document.getElementById("checkpassword").style.color="red";
-        document.getElementById("checkpassword").style.fontSize="14px";
-		document.getElementById("checkpassword").innerHTML="Password too short (6 chars. min)";
-		document.getElementById("password").style.borderColor="red";
-	    aa=0;
-	}else{
-		document.getElementById("checkpassword").style.color="green";
-        document.getElementById("checkpassword").style.fontSize="14px";
-		document.getElementById("checkpassword").innerHTML="✔";
-		document.getElementById("password").style.borderColor="rgb(29,200,287)";
-	}
+    if (password.length < 6) {
+        document.getElementById("checkpassword").style.color = "red";
+        document.getElementById("checkpassword").style.fontSize = "14px";
+        document.getElementById("checkpassword").innerHTML = "Password too short (6 chars. min)";
+        document.getElementById("password").style.borderColor = "red";
+        aa = 0;
+    } else {
+        document.getElementById("checkpassword").style.color = "green";
+        document.getElementById("checkpassword").style.fontSize = "14px";
+        document.getElementById("checkpassword").innerHTML = "✔";
+        document.getElementById("password").style.borderColor = "rgb(29,200,287)";
+    }
 
-	var passwordconfirm=document.getElementById("password_confirmation").value;
+    var passwordconfirm = document.getElementById("password_confirmation").value;
 
-	if(passwordconfirm!=password){
-		document.getElementById("checkpasswordconfirm").style.color="red";
-		document.getElementById("checkpasswordconfirm").style.fontSize="14px";
-		document.getElementById("checkpasswordconfirm").innerHTML="Password mismatch!";
-		document.getElementById("password_confirmation").style.borderColor="red";
-		aa=0;
-	}else if(passwordconfirm=="") {
-		document.getElementById("password_confirmation").style.borderColor="red";
-		aa=0;
-    }else{
-            document.getElementById("checkpasswordconfirm").style.color="green";
-            document.getElementById("checkpasswordconfirm").style.fontSize="14px";
-            document.getElementById("checkpasswordconfirm").innerHTML="✔";
-            document.getElementById("password_confirmation").style.borderColor="rgb(29,200,287)";
+    if (passwordconfirm != password) {
+        document.getElementById("checkpasswordconfirm").style.color = "red";
+        document.getElementById("checkpasswordconfirm").style.fontSize = "14px";
+        document.getElementById("checkpasswordconfirm").innerHTML = "Password mismatch!";
+        document.getElementById("password_confirmation").style.borderColor = "red";
+        aa = 0;
+    } else if (passwordconfirm == "") {
+        document.getElementById("password_confirmation").style.borderColor = "red";
+        aa = 0;
+    } else {
+        document.getElementById("checkpasswordconfirm").style.color = "green";
+        document.getElementById("checkpasswordconfirm").style.fontSize = "14px";
+        document.getElementById("checkpasswordconfirm").innerHTML = "✔";
+        document.getElementById("password_confirmation").style.borderColor = "rgb(29,200,287)";
 
-        }
+    }
 
 
-	var username =document.getElementById("username").value;
-	if(username==""){
-		document.getElementById("checkusername").style.color="red";
-		document.getElementById("checkusername").style.fontSize="14px";
-		document.getElementById("checkusername").innerHTML="Please enter username!";
-		document.getElementById("username").style.borderColor="red";
-		aa=0;
-	}else{
-		document.getElementById("checkusername").style.color="green";
-        document.getElementById("checkusername").style.fontSize="14px";
-		document.getElementById("checkusername").innerHTML="✔";
-		document.getElementById("username").style.borderColor="rgb(29,200,287)";
-	}
+    var username = document.getElementById("username").value;
+    var usern = /^[0-9a-z_]{1,}$/;
+    if (!usern.test(username)) {
+        document.getElementById("checkusername").style.color = "red";
+        document.getElementById("checkusername").style.fontSize = "14px";
+        document.getElementById("checkusername").innerHTML = "Please enter username!";
+        document.getElementById("username").style.borderColor = "red";
+        aa = 0;
+    } else {
+        document.getElementById("checkusername").style.color = "green";
+        document.getElementById("checkusername").style.fontSize = "14px";
+        document.getElementById("checkusername").innerHTML = "✔";
+        document.getElementById("username").style.borderColor = "rgb(29,200,287)";
+    }
 
 
-	if (aa != 1) {
-		return false;
-	} else {
-		return true;
-	}
+    if (aa != 1) {
+        return false;
+    } else {
+        return true;
+    }
 }
